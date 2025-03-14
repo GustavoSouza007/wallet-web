887f405d77410ad20215fba226a8b307552dd4de
GustavoSouza007 <gustavosouzatrindade10@gmail.com>
2025-03-14 12:27:58 -0300

    fix: update getFinancesData to use parameter date instead of hardcoded value

--- src/services/request/index.js
+++ src/services/request/index.js
@@ -1,9 +1,9 @@
 import { apiProject } from "../api";
 
-export const getFinancesData = async () => {
+export const getFinancesData = async (date) => {
   try {
     const email = localStorage.getItem("@walletApp_email");
-    const date = "2022-12-15";
+
     const result = await apiProject.get(`finances?date=${date}`, {
       headers: {
         email: email,
